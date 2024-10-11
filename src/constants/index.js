8fba5f20a1c3be3dddf5c2df4b9e47fac50d28d3
pranittgithub <pranitt@gmail.com>
2024-10-11 20:36:07 +0530

    Your commit message

--- src/constants/index.js
+++ src/constants/index.js
@@ -26,7 +26,6 @@ export const clientReviews = [
     id: 1,
     name: 'Emily Johnson',
     position: 'Marketing Director at GreenLeaf',
-    img: 'assets/review1.png',
     review:
       'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
   },
