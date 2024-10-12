e2b2c0e4b8e547e6deb9fe780ae6e0cb244c133a
pranittgithub <pranitt@gmail.com>
2024-10-12 23:41:44 +0530

    deploy commit

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,8 +22,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.NEXT_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.NEXT_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.REACT_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.REACT_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: 'Ampli Dev',
@@ -31,7 +31,7 @@ const Contact = () => {
           to_email: 'amplidevv@gmail.com',
           message: form.message,
         },
-        import.meta.env.NEXT_APP_EMAILJS_PUBLIC_KEY,
+        import.meta.env.REACT_APP_EMAILJS_PUBLIC_KEY,
       )
       .then(
         () => {
